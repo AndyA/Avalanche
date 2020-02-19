07edd75dc88ce68431b9cb6fc1e89867fdd5b66c
Andy Armstrong <andy@hexten.net>
2020-02-19 16:54:38 +0000

    Keep HLS files

---
+++ bin/hls-keep.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+src="www/hls"
+dst="keep"
+
+last=""
+while sleep 1; do
+  while read file; do
+    if [[ $file > $last ]]; then
+      echo "$file"
+      last="$file"
+    fi
+  done < <( find "$src" -mmin +1 | sort )
+done | cpio -pld "$dst"
+
+# vim:ts=2:sw=2:sts=2:et:ft=sh
+
