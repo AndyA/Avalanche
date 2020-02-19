ee7b04b4e52d4a48e2161f540da7b9e80972a1de
Andy Armstrong <andy@hexten.net>
2020-02-19 15:49:29 +0000

    Tweak ffmpeg params

--- bin/hls-live.sh
+++ bin/hls-live.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-set -x
+set -e
+
+cd "$(dirname "$0")/.."
 
 out="www/hls"
 fps=25
 seg=2
+mbits=3
+
+index="$out/index.m3u8"
+segment="$out/seg-%Y%m%d-%H%M%S.ts"
 
 rm -rf "$out"
 mkdir -p "$out"
@@ -12,20 +18,24 @@ mkdir -p "$out"
 raspivid -o -                          \
   -w 1920 -h 1080                      \
   -fps $fps -g $[fps*seg]              \
-  -b 10000000 -t 0 |                   \
+  -b $[mbits*1000000] -t 0 |           \
   ffmpeg                               \
     -nostdin                           \
+    -hide_banner -loglevel panic       \
     -f h264 -i -                       \
     -c copy                            \
     -f hls                             \
     -hls_time $seg                     \
     -hls_list_size 60                  \
-    -hls_wrap 120                      \
     -hls_delete_threshold 1            \
     -hls_flags delete_segments         \
     -hls_start_number_source datetime  \
+    -hls_allow_cache 1                 \
+    -hls_playlist_type event           \
+    -strftime 1                        \
+    -hls_segment_filename "$segment"   \
     -start_number 10                   \
-    -y "$out/index.m3u8"
+    -y "$index"
 
 
 # vim:ts=2:sw=2:sts=2:et:ft=sh
